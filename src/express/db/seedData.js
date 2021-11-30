58bc60513f42cb06fe7974a5e09e7f667747a041
Mark Clements <mclements@Marks-MBP.attlocal.net>
2021-11-30 13:23:26 -0600

    create users table and seed data

--- src/express/db/seedData.js
+++ src/express/db/seedData.js
@@ -2,49 +2,71 @@
 const {
   client,
   // other db methods
-} = require("./index")
+} = require("./index");
 
 async function rebuildDB() {
   try {
-    await client.query(/*sql*/`
+    await client.query(/*sql*/ `
       DROP TABLE IF EXISTS users;
     `);
 
-    await client.query(/*sql*/`
+    await client.query(/*sql*/ `
       CREATE TABLE users(
         id  SERIAL PRIMARY KEY, 
-        username VARCHAR(255) UNIQUE NOT NULL, 
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL, 
         password VARCHAR(255) NOT NULL
       );
-    `)    // drop tables in correct order
+    `); // drop tables in correct order
     // build tables in correct order
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 async function seedData() {
   try {
+    const users = [
+      {
+        email: "kamjamtest@gmail.com",
+        password: "kamjampassword000",
+        name: "KamJam Test",
+      },
+      {
+        email: "kyletest@gmail.com",
+        password: "kyletestpassword123",
+        name: "Kyle Test",
+      },
+      {
+        email: "austintest@gmail.com",
+        password: "austintestpassword456",
+        name: "Austin Test",
+      },
+      {
+        email: "marktest@gmail.com",
+        password: "marktestpassword789",
+        name: "Mark Test",
+      },
+    ];
 
-  const users = [
-    { username: "testuser", password: "testuser999" }
-  ];
-
-  for(const user of users) {
-    await client.query(/*sql*/`
+    for (const user of users) {
+      await client.query(
+        /*sql*/ `
       INSERT INTO users
-      (username, password)
-      VALUES ($1, $2);
-    `,[user.username, user.password]);
-  }
+      (email, password, name)
+      VALUES ($1, $2, $3);
+    `,
+        [user.email, user.password, user.name]
+      );
+    }
 
-  // create useful starting data
+    // create useful starting data
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 module.exports = {
   rebuildDB,
   seedData,
-}
+};
