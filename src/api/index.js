5b489941752dbce458f835c7ebd51f3e83ac37cd
markclements225 <mcleme9@gmail.com>
2021-12-01 13:06:43 -0600

    fixed folders for users api

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,11 @@
-const express = require("express");
-const apiRouter = express.Router();
+import axios from "axios";
 
-apiRouter.use((req, res, next) => {
-  if (req.user) {
-    console.log("User is set:", req.user);
+// Sample function to call our backend API
+export async function getMessage() {
+  try {
+    const { data } = await axios.get("/api");
+    return data;
+  } catch (error) {
+    throw error;
   }
-
-  next();
-});
-
-const usersRouter = require("./users");
-apiRouter.use("/users", usersRouter);
-
-apiRouter.use((error, req, res, next) => {
-  res.status(500).send(error);
-});
-
-module.exports = apiRouter;
-
-// import axios from 'axios';
-
-// // Sample function to call our backend API
-// export async function getMessage() {
-//   try {
-//     const { data } = await axios.get('/api');
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+}
--- src/express/routes/apiRouter.js
+++ src/express/routes/apiRouter.js
@@ -1,10 +1,31 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
 
 // GET /api
+
+const express = require("express");
+const apiRouter = express.Router();
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+const usersRouter = require("../express/routes/users");
+apiRouter.use("/users", usersRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.status(500).send(error);
+});
+
+module.exports = apiRouter;
+
 // This is a sample route
 apiRouter.get("/", (req, res, next) => {
   res.send({
-    message: "API is under construction!"
+    message: "API is under construction!",
   });
 });
 
