8f9d8c0010d9d051d7fba2253c1756d677d716f0
Kyle Mahnke <kylemahnke@kyles-mbp.attlocal.net>
2021-12-14 09:54:02 -0600

    updated home page css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,12 @@
 //all images need to be resized and links pointed to routers when completed
 
 import React from "react";
+import accTile from "./images/accTile.jpeg";
+import bandTile from "./images/bandTile.jpeg";
+import clusterTile from "./images/clusterTile.jpeg";
+import drumTile from "./images/drumTile.jpeg";
+import guitarTile from "./images/guitarTile.jpeg";
+import keysTile from "./images/keysTile.jpeg";
 
 const Home = () => {
     return (
@@ -9,29 +15,29 @@ const Home = () => {
           <div className="homepage">
               <h1 className="hometitle">Welcome to KamJam</h1>
               {/* replace test links below */}
-              <a href="https://www.fender.com/en-US/electric-guitars/telecaster/jason-isbell-custom-telecaster/0140320364.html">
-                  <img src="https://photos.app.goo.gl/Er5T4pYjd7yqyzns6" class="home-img"/>
-                  <h2>Guitars</h2>
+              {/* guitars */}
+              <a href="/guitars/products">
+              <img src={guitarTile} className="homepageTiles"/>
               </a>
-              <a href="https://en.wikipedia.org/wiki/Drum">
-                  <img src="https://imgur.com/uuqKFOR" class="home-img"/>
-                  <h2>Drums</h2>
+              {/* drums */}
+              <a href="/drums/products">
+              <img src={drumTile} className="homepageTiles"/>
               </a>
-              <a href="https://en.wikipedia.org/wiki/Orchestra">
-                  <img src="https://www.conn-selmer.com/application/files/8815/0722/9036/18037.png" class="home-img"/>
-                  <h2>Band and Orchestra</h2>
+              {/* Band and orchestra */}
+              <a href="/band/products">
+              <img src={bandTile} className="homepageTiles"/>
               </a>
-              <a href="https://www.logitech.com/en-us/products/keyboards.html">
-                  <img src="https://www.nordkeyboards.com/sites/default/files/files/products/nord-stage-3/images/models-stage3-88-revb.jpg" class="home-img"/>
-                  <h2>Keyboards and Synths</h2>
+              {/* keys and synths */}
+              <a href="/keyboards/products">
+              <img src={keysTile} className="homepageTiles"/>
               </a>
-              <a href="https://www.boss.info/us/products/db-90/">
-                  <img src="https://media.guitarcenter.com/is/image/MMGS7/213017000000000-00-2000x2000.jpg" class="home-img"/>
-                  <h2>Accessories</h2>
+              {/* Accesories */}
+              <a href="/accessories/products">
+                  <img src={accTile} className="homepageTiles"/>
               </a>
-              <a href="https://en.wikipedia.org/wiki/The_Beatles">
-                  <img src="https://www.yamaha.com/yamahavgn/PIM/Images/CEL-56P_540x540_735x735_d5dd4b16e6705e566f63a43cc7c0af46.jpg" class="home-img"/>
-                  <h2>See All Products</h2>
+              {/* all products */}
+              <a href="/products">
+              <img src={clusterTile} className="homepageTiles"/>
               </a>
           </div>
         </>
