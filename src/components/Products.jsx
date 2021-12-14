f8c7d3fadd1365dd2f5e0b42c64405d542a345ec
ItsALamb <austinmillerlambert@gmail.com>
2021-12-14 09:51:55 -0600

    adds some styles to products pages

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,16 +6,16 @@ const Products = ({ title, products }) => {
 
     return (
         <>
-            <h1>{title}</h1>
+            <h1 className="product-title">{title}</h1>
             <div className="product-container">
                 {products.map((product) => (
-                    <div className="product-card" key={product.id}>
+                    <div className="product-card" key={product.id} onClick={() => { history.push(`/products/${product.id}`) }}>
                         <h3 className="product-name">{product.name}</h3>
                         <img className="product-image" src={product.imageurl} />
-                        <p className="price">$ {product.price}</p>
                         <div className="product-buttons">
-                            <AddToCartButton />
                             <button type="button" className="details-button" onClick={() => { history.push(`/products/${product.id}`) }}>See Details</button>
+                            <AddToCartButton />
+                            <p className="price">$ {product.price}</p>
                         </div>
                     </div>
                 ))}
